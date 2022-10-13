d01974b7736f085611f7c06c29daa686db37e888
Samb <samb@townsware.com>
2022-10-13 19:25:46 -0400

    Initial comments with name, class and project

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+"""
+(5) Initial comments with your name, class and project at the top of your .py file:
+INF601 - Advanced Programming in Python
+Sam Boutros
+Mini Project 3
+10/23/2022
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
