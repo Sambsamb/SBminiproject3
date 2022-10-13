ff63b387c72056c343add7ce10e00f87024b9e8e
Samb <samb@townsware.com>
2022-10-13 19:23:11 -0400

    db.py initial commit

--- bigfoot/db.py
+++ bigfoot/db.py
@@ -5,11 +5,16 @@ from flask import current_app, g
 
 
 def get_db():
+    # g is a special object that is unique for each request.
     if 'db' not in g:
+        # sqlite3.connect() establishes a connection to the file pointed at by the DATABASE configuration key.
         g.db = sqlite3.connect(
+            # current_app is another special object that points to the Flask application handling the request.
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
+        # sqlite3.Row tells the connection to return rows that behave like dicts.
+        # This allows accessing the columns by name.
         g.db.row_factory = sqlite3.Row
 
     return g.db
